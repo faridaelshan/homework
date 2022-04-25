013ae92fb68ca5fd59889185a3d5837868ec5349
faridaelshan <94470914+faridaelshan@users.noreply.github.com>
2022-04-25 20:20:41 +0400

    Add files via upload

---
+++ steam/assets/js/main.js
@@ -0,0 +1,14 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+darkMode();
+/* const tab1= document.getElementById("tab1");
+const tab2= document.getElementById("tab2");
+const tab3= document.getElementById("tab3");
+const tab4= document.getElementById("tab4");
+const tab5= document.getElementById("tab5"); */
+const tab = document.getElementsByClassName('etiket').value;
+if(tab1){
+tav
+}
