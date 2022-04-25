9c69e5fcc0bc785bd5a4a654206a8cdffc6d7a66
faridaelshan <94470914+faridaelshan@users.noreply.github.com>
2022-04-25 19:38:39 +0400

    Add files via upload

---
+++ Baldini/assets/js/main.js
@@ -0,0 +1,6 @@
+/* const text = document.createElement("p");
+const node = document.createTextNode("Hello");
+text.appendChild(node);
+const element = document.getElementById("div1");
+const child = document.getElementById("p");
+element.insertBefore(text,child); */
